7b237fce7d05ade75c23b667a4f30769d8c60760
Mitch <MitchelClark1997@outlook.com>
2021-08-19 21:06:29 +1000

    added timeout function for auto slide

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # project-template
 
 Got Basic webpack, ESLint and Prettier set up.
+
+To fix prettier not formatting on initialisation error. I enabled it off and on again in the VSCode settings. AND, i added /node_modules to the prettierignore... one of these two seemed to work lol..
+
+Had a heavy helping hand from this <a href='https://www.w3schools.com/howto/howto_js_slideshow.asp'>w3schools</a> article.
--- src/index.js
+++ src/index.js
@@ -1,1 +1,82 @@
 import "./style.scss";
+
+import img1 from "./flicks/img1.jpg";
+import img2 from "./flicks/img2.jpg";
+import img3 from "./flicks/img3.jpg";
+import img4 from "./flicks/img4.jpg";
+import img5 from "./flicks/img5.jpg";
+
+const imgSli = document.querySelectorAll(".slideshow-container");
+
+function addImg(img) {
+  const ele = document.createElement("div");
+  const myIcon = new Image();
+
+  ele.classList.add("mySlides", "fade", "hide");
+
+  myIcon.src = img;
+
+  ele.appendChild(myIcon);
+
+  return ele;
+}
+
+imgSli[0].appendChild(addImg(img1));
+imgSli[0].appendChild(addImg(img2));
+imgSli[0].appendChild(addImg(img3));
+imgSli[0].appendChild(addImg(img4));
+imgSli[0].appendChild(addImg(img5));
+
+const nextArrow = document.querySelectorAll(".next");
+const prevArrow = document.querySelectorAll(".prev");
+const dots = document.querySelectorAll(".dot");
+
+let slideIndex = 0;
+
+function showSlides(n) {
+  let i;
+  const slides = document.querySelectorAll(".mySlides");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i += 1) {
+    slides[i].classList.add("hide");
+    dots[i].classList.remove("active");
+  }
+
+  slides[slideIndex - 1].classList.remove("hide");
+  dots[slideIndex - 1].classList.add("active");
+}
+
+function autoSlide() {
+  slideIndex += 1;
+  showSlides(slideIndex);
+  setTimeout(autoSlide, 5000);
+}
+
+autoSlide();
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+nextArrow[0].addEventListener("click", () => {
+  plusSlides(1);
+});
+
+prevArrow[0].addEventListener("click", () => {
+  plusSlides(-1);
+});
+
+dots.forEach((dot, i) => {
+  dot.addEventListener("click", () => {
+    currentSlide(i + 1);
+  });
+});
